425c72b17241991f73af917951ac017f78da0d2b
JoaoFPessoa <joaoppessoa@uol.com.br>
2025-02-05 18:38:33 -0300

    add: more configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,31 @@ export default {
   theme: {
     extend: {
       colors: {
+        pink: {
+          DEFAULT: "#ffa9da", // Base color
+          light: "#ffbde3", // Lighter pink
+          dark: "#d48bb7", // Darker pink
+        },
+        yellow: {
+          DEFAULT: "#f4ff8a",
+          light: "#f7ffa8",
+          dark: "#cfd768",
+        },
+        ice: {
+          DEFAULT: "#fcfcfc",
+          light: "#ffffff",
+          dark: "#f0f0f0",
+        },
+        gray: {
+          DEFAULT: "#eaeaea",
+          light: "#f5f5f5",
+          dark: "#bfbfbf",
+        },
+        black: {
+          DEFAULT: "#0c0c0c",
+          light: "#1a1a1a",
+          dark: "#000000",
+        },
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         card: {
@@ -25,7 +50,7 @@ export default {
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
+          DEFAULT: "#9CA3AF",
           foreground: "hsl(var(--secondary-foreground))",
         },
         muted: {
@@ -50,11 +75,11 @@ export default {
           "4": "hsl(var(--chart-4))",
           "5": "hsl(var(--chart-5))",
         },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        borderRadius: {
+          lg: "var(--radius)",
+          md: "calc(var(--radius) - 2px)",
+          sm: "calc(var(--radius) - 4px)",
+        },
       },
     },
   },
