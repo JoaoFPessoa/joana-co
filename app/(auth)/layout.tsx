dc2d6e8f867368188254812155efc4e1e7fe9b6a
JoaoFPessoa <joaoppessoa@uol.com.br>
2025-02-05 18:49:21 -0300

    add: responsivity on login page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,7 +18,7 @@ export default function Layout({
       className={`${dmSans.className} h-screen flex flex-col bg-gradient-to-br `}
     >
       {/* right corner simble */}
-      <div className="overflow-hidden -z-10 right-0 absolute inset-0">
+      <div className="hidden md:block overflow-hidden -z-10 right-0 absolute inset-0">
         <div className="absolute -top-20 -right-16">
           <Image
             src={"/logos/simble-pink.svg"}
@@ -29,7 +29,7 @@ export default function Layout({
         </div>
       </div>
       {/* Navbar */}
-      <nav className="p-4 text-white flex items-center">
+      <nav className="p-4 text-white text-center flex items-center justify-center md:justify-normal">
         {/* blurred pink */}
         <div className="absolute -z-10 top-0 left-0 bg-[#ffa9da52] w-[250px] h-[250px] rounded-full rotate-[32.46deg] blur-[50px]" />
         {/* logo */}
@@ -44,9 +44,9 @@ export default function Layout({
       </nav>
 
       {/* Main content */}
-      <div className="flex flex-1 ">
+      <div className="flex flex-col  items-center md:flex-row flex-1 ">
         {/* Left section */}
-        <div className="w-1/2  flex   items-center justify-center">
+        <div className="hidden md:flex w-1/2   items-center justify-center">
           <div className="flex flex-col max-w-[350px] gap-10">
             <Image
               src={"/login-assets/aspas.svg"}
@@ -69,7 +69,7 @@ export default function Layout({
         </div>
 
         {/* Right section (children) */}
-        <div className="w-1/2 p-6   flex flex-col items-center justify-center">
+        <div className="md:w-1/2 p-6   flex flex-col items-center justify-center">
           {children}
         </div>
       </div>
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import AuthTabs from "./AuthTabs";
 
 export default function LoginPage() {
   return (
-    <Card className="border-0 flex  w-full  max-w-md h-full ">
+    <Card className="border-0 flex  w-full md:mt-0  h-full ">
       <CardContent className="flex justify-center    flex-col gap-10 h-full">
         <h1 className="text-4xl font-bold text-center tracking-tight">
           Boas-vindas ao Joana.co*
