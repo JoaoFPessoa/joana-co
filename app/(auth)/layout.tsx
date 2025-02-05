afcbcdd3ddbede30506585dce02bae3b298f87d4
JoaoFPessoa <joaoppessoa@uol.com.br>
2025-02-05 18:38:51 -0300

    fixed login layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -17,10 +17,21 @@ export default function Layout({
     <div
       className={`${dmSans.className} h-screen flex flex-col bg-gradient-to-br `}
     >
+      {/* right corner simble */}
+      <div className="overflow-hidden -z-10 right-0 absolute inset-0">
+        <div className="absolute -top-20 -right-16">
+          <Image
+            src={"/logos/simble-pink.svg"}
+            alt=""
+            width={260}
+            height={260}
+          />
+        </div>
+      </div>
       {/* Navbar */}
       <nav className="p-4 text-white flex items-center">
         {/* blurred pink */}
-        <div className="absolute top-0 left-0 bg-[#ffa9da52] w-[250px] h-[250px] rounded-full rotate-[32.46deg] blur-[50px]" />
+        <div className="absolute -z-10 top-0 left-0 bg-[#ffa9da52] w-[250px] h-[250px] rounded-full rotate-[32.46deg] blur-[50px]" />
         {/* logo */}
         <Link href="/">
           <Image
@@ -33,9 +44,9 @@ export default function Layout({
       </nav>
 
       {/* Main content */}
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex flex-1 ">
         {/* Left section */}
-        <div className="w-1/2  flex  items-center justify-center">
+        <div className="w-1/2  flex   items-center justify-center">
           <div className="flex flex-col max-w-[350px] gap-10">
             <Image
               src={"/login-assets/aspas.svg"}
@@ -58,7 +69,7 @@ export default function Layout({
         </div>
 
         {/* Right section (children) */}
-        <div className="w-1/2 p-6  flex flex-col items-center justify-center">
+        <div className="w-1/2 p-6   flex flex-col items-center justify-center">
           {children}
         </div>
       </div>
